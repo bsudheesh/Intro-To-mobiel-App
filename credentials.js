ccdd846aae0ec4ebcd2464036392a97831f8b578
bsudheesh <sudheesh.bhattarai@bison.howard.edu>
2015-10-02 16:37:58 -0400

    Create credentials.js

---
+++ credentials.js
@@ -0,0 +1,4 @@
+PARSE__HEADER_CREDENTIALS = {
+        "x-parse-application-id": "m0UXUGU0VJ1icR6FQlYEeFxgRdZONTO1oki4qHeh",
+        "x-parse-rest-api-key": "g2GbZ76brjGDQtyXakJ4lkKsHTcwSvOqAuuRZJ40"
+    };
